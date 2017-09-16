8a7b1bafd46b2d624636a3660c2ed95ad4654a39
Matt <stygianvision@gmail.com>
2017-09-15 19:39:35 -0700

    Source

---
+++ src/EventDispatcher.ts
@@ -0,0 +1,107 @@
+
+export interface EventCallbackList {
+	[index: string]: EventCallback[];
+}
+
+
+export class EventCallback {
+    public calls: number;
+    constructor(
+        public fnc: any,
+        public key: number,
+        public once: boolean = false,
+        public context?: any,
+    ) {
+        this.calls = 0;
+    }
+
+    call(args): boolean {
+        if(this.once && this.calls > 0)
+            return false;
+
+        this.fnc.apply(this.context, args);
+        this.calls += 1;
+        return true;
+    }
+}
+
+
+export class EventDispatcher  {
+    private _listeners: EventCallbackList;
+    private _lastKey: number;
+
+    constructor() {
+        this._lastKey = 0;
+        this._listeners = {};
+    }
+
+    bind(event: string, fct: () => any, context?: any, once?: boolean): number {
+        once = once || false;
+        this._lastKey++;
+        this._listeners[event] = this._listeners[event] || [];
+        this._listeners[event].push(new EventCallback(fct, this._lastKey, once, context));
+        return this._lastKey;
+    }
+
+    once(event: string, fct: () => any, context?: any): number {
+        return this.bind(event, fct, context, true);
+    }
+
+    unbind(event: string, key?: number): boolean {
+        if(event in this._listeners === false) return false;
+        if(key) {
+            for(const cb of this._listeners[event]) {
+                if(key == cb.key) {
+                    this._listeners[event].splice(this._listeners[event].indexOf(cb), 1);
+                    return true;
+                }
+            }
+        } else {
+            this._listeners[event] = [];
+            return true;
+        }
+        return false;
+    }
+
+    unbindWithContext(event: string, context: any): number {
+        if(event in this._listeners === false) return 0;
+        let toRemove: EventCallback[] = [],
+            cnt = 0;
+
+        for(const cb of this._listeners[event]) {
+            if(context == cb.context) {
+                toRemove.push(cb);
+            }
+        }
+
+        for(const cb of toRemove) {
+            this._listeners[event].splice(this._listeners[event].indexOf(cb), 1);
+            cnt++;
+        }
+        return cnt;
+    }
+
+    getListener(event: string, key: number): EventCallback {
+        for(const cb of this._listeners[event]) {
+            if(key == cb.key)
+                return cb;
+        }
+    }
+
+    trigger(event: string, ...args: any[]): void {
+        if(event in this._listeners === false) return;
+
+        // Loop through the list backwards so we can pop off .once callbacks
+        for(let i = this._listeners[event].length - 1; i >= 0; i--) {
+            const cb = this._listeners[event][i];
+
+            // We need to unbind callbacks before they're called to prevent
+            // infinite loops if the event is somehow triggered within the
+            // callback
+            if(cb.once)
+                this.unbind(event, cb.key);
+
+            cb.call.call(cb, args);
+        }
+    }
+}
---
+++ test/dispatcher.spec.ts
@@ -0,0 +1,139 @@
+import { EventDispatcher } from "../src/EventDispatcher";
+
+
+describe('EventDispatcher', () => {
+    let dispatcher: EventDispatcher = null,
+        dummy = null,
+        dummy2 = null;
+
+    beforeEach(() => {
+        dispatcher = new EventDispatcher();
+        dummy = jasmine.createSpyObj('dummy', ['callback']);
+        dummy2 = jasmine.createSpyObj('dummy2', ['callback']);
+    });
+
+    it("should bind and be called properly", () => {
+        dispatcher.bind('event', dummy.callback.bind(dummy));
+        expect(dummy.callback).not.toHaveBeenCalled();
+        dispatcher.trigger('event', 1, 2, 'three');
+        expect(dummy.callback).toHaveBeenCalled();
+    });
+
+    it("should unbind and not be called", () => {
+        const key: number = dispatcher.bind('event', dummy.callback.bind(dummy));
+        expect(dispatcher.unbind('event', key)).toEqual(true);
+        dispatcher.trigger('event', 1, 2, 'three');
+        expect(dummy.callback).not.toHaveBeenCalled();
+    });
+
+    it("should not unbind events that don't exist", () => {
+        const key: number = dispatcher.bind('event', dummy.callback.bind(dummy));
+        expect(dispatcher.unbind('fake_event', -1)).toEqual(false);
+        dispatcher.trigger('event', 1, 2, 'three');
+        expect(dummy.callback).toHaveBeenCalled();
+    });
+
+    it("should bind and be called properly", () => {
+        dispatcher.bind('event', dummy.callback.bind(dummy));
+        expect(dummy.callback).not.toHaveBeenCalled();
+        dispatcher.trigger('event', 1, 2, 'three');
+        expect(dummy.callback).toHaveBeenCalled();
+    });
+
+    it("should unbind all", () => {
+        dispatcher.bind('event', dummy.callback, dummy);
+        dispatcher.bind('event', dummy.callback, dummy);
+        dispatcher.bind('event', dummy2.callback, dummy2);
+        expect(dispatcher.unbind('event')).toEqual(true);
+        expect(dispatcher.unbindWithContext('event', dummy)).toBe(0);
+        expect(dispatcher.unbindWithContext('event', dummy2)).toBe(0);
+        dispatcher.trigger('event');
+        expect(dummy.callback).not.toHaveBeenCalled();
+    });
+
+    it("should unbind all with matching context", () => {
+        dispatcher.bind('event', dummy.callback, dummy);
+        dispatcher.bind('event', dummy.callback, dummy);
+        dispatcher.bind('event', dummy2.callback, dummy2);
+        expect(dispatcher.unbindWithContext('event', dummy)).toBe(2);
+        dispatcher.trigger('event');
+        expect(dummy.callback).not.toHaveBeenCalled();
+    });
+
+    it("should unbind the correct listeners by key", () => {
+        let key1 = dispatcher.bind('event', dummy.callback, dummy),
+            key2 = dispatcher.bind('event', dummy.callback, dummy),
+            key3 = dispatcher.bind('event', dummy.callback, dummy),
+            key4 = dispatcher.bind('event', dummy.callback, dummy),
+            key5 = dispatcher.bind('event', dummy.callback, dummy);
+
+        expect(dispatcher.unbind('event', key2)).toBe(true);
+        expect(dispatcher.unbind('event', key4)).toBe(true);
+        expect(dispatcher.unbind('event', key1)).toBe(true);
+        expect(dispatcher.unbind('event', key3)).toBe(true);
+        expect(dispatcher.unbind('event', key5)).toBe(true);
+    });
+
+    it("should fail to unbind the once listeners", () => {
+        let key1 = dispatcher.once('event', dummy.callback, dummy),
+            cb1 = dispatcher.getListener('event', key1),
+            key2 = dispatcher.once('event', dummy.callback, dummy),
+            cb2 = dispatcher.getListener('event', key2),
+            key3 = dispatcher.once('event', dummy.callback, dummy),
+            cb3 = dispatcher.getListener('event', key3),
+            key4 = dispatcher.bind('event', dummy.callback, dummy),
+            cb4 = dispatcher.getListener('event', key4),
+            key5 = dispatcher.once('event', dummy.callback, dummy),
+            cb5 = dispatcher.getListener('event', key5);
+
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        expect(dispatcher.unbind('event', key2)).toBe(false);
+        expect(cb2.calls).toBe(1);
+        expect(dispatcher.unbind('event', key4)).toBe(true);
+        expect(cb4.calls).toBe(4);
+        expect(dispatcher.unbind('event', key1)).toBe(false);
+        expect(cb1.calls).toBe(1);
+        expect(dispatcher.unbind('event', key3)).toBe(false);
+        expect(cb3.calls).toBe(1);
+        expect(dispatcher.unbind('event', key5)).toBe(false);
+        expect(cb5.calls).toBe(1);
+        expect(dispatcher.unbind('event', key4)).toBe(false);
+    });
+
+    it("should only be called once", () => {
+        let onceCalls = 0,
+            normalCalls = 0;
+        dispatcher.once('event', () => {
+            // empty
+        });
+        dispatcher.once('event', () => {
+            onceCalls += 1;
+        });
+        dispatcher.once('event', () => {
+            // empty
+        });
+        dispatcher.bind('event', () => {
+            normalCalls += 1;
+        });
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        dispatcher.trigger('event');
+        expect(onceCalls).toBe(1);
+        expect(normalCalls).toBe(4);
+    });
+
+    it("should only be called once with nested event triggers", () => {
+        let onceCalls = 0,
+            normalCalls = 0;
+        const key = dispatcher.once('event', () => {
+                dispatcher.trigger('event');
+            }),
+            cb = dispatcher.getListener('event', key);
+        dispatcher.trigger('event');
+        expect(cb.calls).toBe(1);
+    });
+});
